b35ef51addc97f5af7ce30484063441bb35f6c18
Anderson Silva <adesozasilva@gmail.com>
2020-06-13 15:38:41 -0300

    fist commit

---
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -0,0 +1,30 @@
+package br.com.alura.forum.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.model.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
+
+@RestController
+public class TopicosController {
+
+	@Autowired
+	TopicoRepository topicoRepository;
+	
+	@RequestMapping("/topicos")
+	public List<TopicoDto> lista(String nomeCurso){
+		List<Topico> topicos;
+		if(nomeCurso == null) {
+			topicos = topicoRepository.findAll();
+		}
+		else {
+			topicos = topicoRepository.findByCursoNome(nomeCurso);
+		}
+		return TopicoDto.converter(topicos);
+	}
+}
